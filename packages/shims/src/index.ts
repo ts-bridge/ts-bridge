946c552ff6a439d20cf96814082aa1441a6bb0a8
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-08-28 11:52:35 +0200

    Remove shims package (#38)
    This simply removes the entire shims package, as it's no longer used.
    Once the new version of the CLI is published, I will mark it as
    deprecated on NPM.

--- packages/shims/src/index.ts
+++
@@ -1,5 +0,0 @@
-// We export the CommonJS shims from the index file so that they can be imported
-// from the package root. This is useful for consumers who are using the package
-// in an older environments that don't support the exports field in the
-// `package.json`, which usually don't support ESM in the first place.
-export * from './commonjs.js';
