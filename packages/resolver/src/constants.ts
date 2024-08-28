1b1d1858a4f007c16bc63353df4d31ed88f6beba
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2024-08-28 16:58:39 +0200

    Remove CommonJS from default conditions

--- packages/resolver/src/constants.ts
+++ packages/resolver/src/constants.ts
@@ -2,4 +2,4 @@
  * Default conditions for the resolver. These are the fields that are checked in
  * order to resolve an import from the `package.json` exports field.
  */
-export const DEFAULT_CONDITIONS = ['node', 'import', 'require'];
+export const DEFAULT_CONDITIONS = ['node', 'import'];
