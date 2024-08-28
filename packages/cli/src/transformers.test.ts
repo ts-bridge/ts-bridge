ts-bridge/ts-bridge

commit ea90026cc43e2a153377472ebbe78231746fa2cc

Transform default CommonJS imports (#19)
This adds a new helper function for default CommonJS imports. The helper
function `importDefault` is based on `tsc`s helper function, and solves
default imports from ESM of a CJS module which specifies `__esModule`.
For example, the output of `tsc` for a CommonJS module with a default
export may be something like this (reformatted):

```ts
const __importDefault =
  (this && this.__importDefault) ||
  function (mod) {
    return mod && mod.__esModule ? mod : { default: mod };
  };

Object.defineProperty(exports, '__esModule', { value: true });
exports.default = void 0;

const foo_1 = require('./foo');

Object.defineProperty(exports, 'default', {
  enumerable: true,
  get() {
    return __importDefault(foo_1).default;
  },
});
```

TypeScript sets `__esModule` to `true` and then exports the default
value as `exports.default` rather than just `exports`. When importing
this in ESM normally, this results in the following:

```ts
import foo from 'cjs-module';

console.log(foo); // { default: 'value' }
```

After this change, each default import from a CJS module will be changed
to use the `importDefault` helper, so the resulting code looks something
like this instead:

```ts
function $importDefault(module) {
  // ...;
}

import $foo from 'cjs-module';
const foo = $importDefault($foo);

console.log(foo); // 'value'
```

packages/cli/src/transformers.test.ts
@@ -14,6 +14,7 @@ import type { BuildType } from './build-type.js';
 import { getBuildTypeOptions } from './build-type.js';
 import { getTypeScriptConfig } from './config.js';
 import {
+  getDefaultImportTransformer,
   getExportExtensionTransformer,
   getGlobalsTransformer,
   getImportAttributeTransformer,
@@ -684,6 +685,72 @@ describe('getImportMetaTransformer', () => {
         }
         // @ts-expect-error - \`import.meta.url\` isn't allowed here.
         console.log($getImportMetaUrl(__filename));
+        "
+      `);
+    });
+  });
+});
+
+describe('getDefaultImportTransformer', () => {
+  describe('when targeting `module`', () => {
+    let files: Record<string, string>;
+
+    beforeAll(() => {
+      const compiler = createCompiler(getFixture('default-imports'));
+      files = compiler(
+        'module',
+        getDefaultImportTransformer({
+          typeChecker: compiler.typeChecker,
+          system: sys,
+        }),
+      );
+    });
+
+    it('rewrites a default import for a CommonJS module import', async () => {
+      expect(files['default-import.js']).toMatchInlineSnapshot(`
+        "function $importDefault(module) {
+            if (module?.__esModule) {
+                return module.default;
+            }
+            return module;
+        }
+        import $foo from 'commonjs-module';
+        const foo = $importDefault($foo);
+        console.log(foo);
+        "
+      `);
+    });
+
+    it('only rewrites default imports', async () => {
+      expect(files['multiple-imports.js']).toMatchInlineSnapshot(`
+        "function $importDefault(module) {
+            if (module?.__esModule) {
+                return module.default;
+            }
+            return module;
+        }
+        import $foo from 'commonjs-module';
+        const foo = $importDefault($foo);
+        import { foo as bar } from 'commonjs-module';
+        import baz from 'es-module';
+        console.log(foo, bar, baz);
+        "
+      `);
+    });
+
+    it('does not rewrite an invalid import', async () => {
+      expect(files['invalid.js']).toMatchInlineSnapshot(`
+        "// @ts-expect-error - Invalid module specifier.
+        import foo from bar;
+        foo;
+        "
+      `);
+    });
+
+    it('does not rewrite an ESM import', async () => {
+      expect(files['es-module-import.js']).toMatchInlineSnapshot(`
+        "import foo from 'es-module';
+        console.log(foo);
         "
       `);
     });

packages/cli/src/build-type.ts
@@ -7,6 +7,7 @@ import typescript from 'typescript';
 
 import type { TransformerOptions } from './transformers.js';
 import {
+  getDefaultImportTransformer,
   getRemoveImportAttributeTransformer,
   getImportAttributeTransformer,
   getRequireTransformer,
@@ -60,6 +61,7 @@ export const BUILD_TYPES: Record<BuildType, BuildTypeOptions> = {
     sourceExtension: '.mts',
     target: ModuleKind.ESNext,
     getTransformers: (options) => [
+      getDefaultImportTransformer(options),
       getNamedImportTransformer(options),
       getImportAttributeTransformer(
         {

packages/cli/src/build.test.ts
@@ -540,7 +540,7 @@ describe('getTransformers', () => {
       true,
     );
 
-    expect(transformers).toHaveLength(5);
+    expect(transformers).toHaveLength(6);
   });
 
   it('returns the correct transformers for the `commonjs` format', () => {
@@ -568,7 +568,7 @@ describe('getTransformers', () => {
       false,
     );
 
-    expect(transformers).toHaveLength(3);
+    expect(transformers).toHaveLength(4);
   });
 
   it('returns the correct transformers for the `commonjs` format without shims', () => {

packages/cli/src/helpers.test.ts
@@ -1,0 +1,52 @@
+import { getVirtualEnvironment } from '@ts-bridge/test-utils';
+import type { Statement } from 'typescript';
+import { factory } from 'typescript';
+import { describe, expect, it } from 'vitest';
+
+import { getImportDefaultHelper } from './helpers.js';
+
+/**
+ * Compile a statement. This is used to test the output of the shim functions.
+ *
+ * @param node - The statement to compile.
+ * @returns The compiled code.
+ */
+function compile(node: Statement | Statement[]) {
+  const { program, system } = getVirtualEnvironment({
+    files: {
+      '/index.ts': '// no-op',
+    },
+  });
+
+  const statements = Array.isArray(node) ? node : [node];
+  program.emit(undefined, undefined, undefined, false, {
+    before: [
+      () => {
+        return (sourceFile) => {
+          // TypeScript doesn't provide a nice way to add a new source file, so
+          // instead we update the dummy source file with the new node.
+          return factory.updateSourceFile(sourceFile, statements);
+        };
+      },
+    ],
+  });
+
+  return system.readFile('/index.js');
+}
+
+describe('getImportDefaultHelper', () => {
+  it('returns the `importDefault` helper function', () => {
+    const ast = getImportDefaultHelper('importDefault');
+
+    expect(compile(ast)).toMatchInlineSnapshot(`
+      ""use strict";
+      function importDefault(module) {
+          if (module?.__esModule) {
+              return module.default;
+          }
+          return module;
+      }
+      "
+    `);
+  });
+});

packages/cli/src/helpers.ts
@@ -1,0 +1,70 @@
+import typescript from 'typescript';
+
+const { factory, SyntaxKind } = typescript;
+
+/**
+ * Get the AST for the `importDefault` helper function, i.e.:
+ *
+ * ```ts
+ * function importDefault(module?: any): any {
+ *   if (module?.__esModule) {
+ *     return module.default;
+ *   }
+ *
+ *   return module;
+ * }
+ * ```
+ *
+ * This is based on the `__importDefault` function that TypeScript generates
+ * when compiling ES modules to CommonJS. It is used to import the default
+ * export from a (CommonJS) module. This checks if the module has a `__esModule`
+ * property, and if so, returns `module.default`. Otherwise, it returns the
+ * module itself.
+ *
+ * @param functionName - The name of the function.
+ * @returns The AST for the `importDefault` helper function.
+ */
+export function getImportDefaultHelper(functionName: string) {
+  return factory.createFunctionDeclaration(
+    undefined,
+    undefined,
+    factory.createIdentifier(functionName),
+    undefined,
+    [
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        factory.createIdentifier('module'),
+        factory.createToken(SyntaxKind.QuestionToken),
+        factory.createKeywordTypeNode(SyntaxKind.AnyKeyword),
+        undefined,
+      ),
+    ],
+    factory.createKeywordTypeNode(SyntaxKind.AnyKeyword),
+    factory.createBlock(
+      [
+        factory.createIfStatement(
+          factory.createPropertyAccessChain(
+            factory.createIdentifier('module'),
+            factory.createToken(SyntaxKind.QuestionDotToken),
+            factory.createIdentifier('__esModule'),
+          ),
+          factory.createBlock(
+            [
+              factory.createReturnStatement(
+                factory.createPropertyAccessExpression(
+                  factory.createIdentifier('module'),
+                  factory.createIdentifier('default'),
+                ),
+              ),
+            ],
+            true,
+          ),
+          undefined,
+        ),
+        factory.createReturnStatement(factory.createIdentifier('module')),
+      ],
+      true,
+    ),
+  );
+}

packages/cli/src/transformers.ts
@@ -21,7 +21,8 @@ import {
   getUniqueIdentifier,
   isGlobal,
 } from './generator.js';
-import { getModulePath, getModuleType } from './module-resolver.js';
+import { getImportDefaultHelper } from './helpers.js';
+import { getModulePath, getModuleType, isCommonJs } from './module-resolver.js';
 import {
   getDirnameGlobalFunction,
   getDirnameHelperFunction,
@@ -42,6 +43,7 @@ const {
   isPropertyAccessExpression,
   isStringLiteral,
   isVariableDeclaration,
+  NodeFlags,
   visitEachChild,
   visitNode,
   SyntaxKind,
@@ -475,6 +477,118 @@ export function getImportMetaTransformer({ typeChecker }: TransformerOptions) {
       if (insertShim) {
         return factory.updateSourceFile(modifiedSourceFile, [
           getImportMetaUrlFunction(functionName),
+          ...modifiedSourceFile.statements,
+        ]);
+      }
+
+      return modifiedSourceFile;
+    };
+  };
+}
+
+/**
+ * Get a transformer that updates the default imports to use the `importDefault`
+ * helper function for CommonJS modules.
+ *
+ * For example, the following default import:
+ *
+ * ```ts
+ * import foo from 'module';
+ * ```
+ *
+ * will be transformed to:
+ *
+ * ```ts
+ * function $importDefault(module) {
+ *   // ...;
+ * }
+ *
+ * import $foo from 'module';
+ * const foo = $importDefault($foo);
+ * ```
+ *
+ * @param options - The transformer options.
+ * @param options.typeChecker - The type checker to use.
+ * @param options.system - The compiler system to use.
+ * @returns The transformer function.
+ */
+export function getDefaultImportTransformer({
+  typeChecker,
+  system,
+}: TransformerOptions) {
+  return (context: TransformationContext): Transformer<SourceFile> => {
+    return (sourceFile: SourceFile) => {
+      let insertShim = false;
+      const importDefaultFunctionName = getUniqueIdentifier(
+        typeChecker,
+        sourceFile,
+        'importDefault',
+      );
+
+      const visitor = (node: Node): Node | Node[] | undefined => {
+        if (isImportDeclaration(node) && node.importClause?.name) {
+          // If the module specifier is not a string literal, return the node as is.
+          if (!isStringLiteral(node.moduleSpecifier)) {
+            return node;
+          }
+
+          // If the module specifier is not a CommonJS module, return the node as is.
+          if (
+            !isCommonJs(node.moduleSpecifier.text, system, sourceFile.fileName)
+          ) {
+            return node;
+          }
+
+          insertShim = true;
+          const name = getUniqueIdentifier(
+            typeChecker,
+            sourceFile,
+            node.importClause.name.text,
+          );
+
+          const importDeclaration = factory.updateImportDeclaration(
+            node,
+            node.modifiers,
+            factory.updateImportClause(
+              node.importClause,
+              node.importClause.isTypeOnly,
+              factory.createIdentifier(name),
+              node.importClause.namedBindings,
+            ),
+            node.moduleSpecifier,
+            node.attributes,
+          );
+
+          const variableDeclaration = factory.createVariableStatement(
+            undefined,
+            factory.createVariableDeclarationList(
+              [
+                factory.createVariableDeclaration(
+                  factory.createIdentifier(node.importClause.name.text),
+                  undefined,
+                  undefined,
+                  factory.createCallExpression(
+                    factory.createIdentifier(importDefaultFunctionName),
+                    undefined,
+                    [factory.createIdentifier(name)],
+                  ),
+                ),
+              ],
+              // eslint-disable-next-line no-bitwise
+              NodeFlags.Const,
+            ),
+          );
+
+          return [importDeclaration, variableDeclaration];
+        }
+
+        return visitEachChild(node, visitor, context);
+      };
+
+      const modifiedSourceFile = visitNode(sourceFile, visitor) as SourceFile;
+      if (insertShim) {
+        return factory.updateSourceFile(modifiedSourceFile, [
+          getImportDefaultHelper(importDefaultFunctionName),
           ...modifiedSourceFile.statements,
         ]);
       }

packages/test-utils/test/fixtures/default-imports/.eslintrc.cjs
@@ -1,0 +1,21 @@
+module.exports = {
+  extends: ['../../../.eslintrc.cjs'],
+
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+  },
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'import-x/extensions': 'off',
+        'import-x/no-duplicates': 'off',
+      },
+    },
+  ],
+
+  ignorePatterns: ['**/invalid.ts'],
+};

packages/test-utils/test/fixtures/default-imports/packages/es-module/.eslintrc.cjs
@@ -1,0 +1,14 @@
+module.exports = {
+  extends: ['../../.eslintrc.cjs'],
+
+  overrides: [
+    {
+      files: ['*.js'],
+      parserOptions: {
+        sourceType: 'module',
+      },
+    },
+  ],
+
+  ignorePatterns: ['**/invalid.ts'],
+};

commit 5128e7fd4c955bb0a9f6629394be3ac865efc271

Re-use existing default import for importing undetected named imports (#40)
This fixes a small bug introduced by #19. The following code for
example:

```ts
import foo, { bar } from 'baz';
```

would be transformed by the default import transformer to:

```ts
import $foo, { bar } from 'baz';
const foo = $importDefault($foo);
```

but the named import transformer would then (assuming `bar` is an
undetected import) transform it to:

```ts
import $baz from 'baz';
const { bar } = $baz;
const foo = $importDefault($foo);
```

where `$foo` now refers to an undeclared variable.

To fix it, I've simply updated the named import transformer to re-use
the name of the default import if it exists. After this change, the code
would be compiled like this instead:

```ts
import $foo from 'baz';
const { bar } = $foo;
const foo = $importDefault($foo);
```

packages/cli/src/generator.ts
@@ -250,11 +250,11 @@ export function getNamedImportNodes(
   }
 
   const moduleSpecifier = getIdentifierName(node.moduleSpecifier.text);
-  const importIdentifier = getUniqueIdentifier(
-    typeChecker,
-    sourceFile,
-    moduleSpecifier,
-  );
+  const importIdentifier =
+    // If the import declaration has a name (default import), use that name, to
+    // avoid breaking the default import transformer.
+    node.importClause.name?.text ??
+    getUniqueIdentifier(typeChecker, sourceFile, moduleSpecifier);
 
   const statements: Statement[] = [];
 

packages/cli/src/transformers.test.ts
@@ -738,6 +738,21 @@ describe('getDefaultImportTransformer', () => {
       `);
     });
 
+    it('only rewrites default imports in combined imports', async () => {
+      expect(files['combined-imports.js']).toMatchInlineSnapshot(`
+        "function $importDefault(module) {
+            if (module?.__esModule) {
+                return module.default;
+            }
+            return module;
+        }
+        import $foo, { foo as bar } from 'commonjs-module';
+        const foo = $importDefault($foo);
+        console.log(foo, bar);
+        "
+      `);
+    });
+
     it('does not rewrite an invalid import', async () => {
       expect(files['invalid.js']).toMatchInlineSnapshot(`
         "// @ts-expect-error - Invalid module specifier.
@@ -807,6 +822,28 @@ describe('getNamedImportTransformer', () => {
         import $commonjsmodule from 'commonjs-module';
         const { bar: barImport } = $commonjsmodule;
         console.log(fooImport, barImport);
+        "
+      `);
+    });
+
+    it('uses an existing default import if it exists', () => {
+      expect(files['combined-imports.js']).toMatchInlineSnapshot(`
+        "import $foo from 'commonjs-module';
+        const { bar } = $foo;
+        /**
+         * Default import helper.
+         *
+         * @param module - Module with default export.
+         * @returns Default export.
+         */
+        function $importDefault(module) {
+            if (module?.__esModule) {
+                return module.default;
+            }
+            return module;
+        }
+        const foo = $importDefault($foo);
+        console.log(foo, bar);
         "
       `);
     });

packages/test-utils/test/fixtures/default-imports/src/combined-imports.ts
@@ -1,0 +1,3 @@
+import foo, { foo as bar } from 'commonjs-module';
+
+console.log(foo, bar);

packages/test-utils/test/fixtures/named-imports/src/combined-imports.ts
@@ -1,0 +1,17 @@
+import $foo, { bar } from 'commonjs-module';
+
+/**
+ * Default import helper.
+ *
+ * @param module - Module with default export.
+ * @returns Default export.
+ */
+function $importDefault(module: any): any {
+  if (module?.__esModule) {
+    return module.default;
+  }
+  return module;
+}
+
+const foo = $importDefault($foo);
+console.log(foo, bar);